753f4b604d8c1a202f2380de497db7c8ac664c55
philenius <philenius@users.noreply.github.com>
2020-08-20 12:05:17 +0200

    change to a dark app theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,44 @@ class MyApp extends StatelessWidget {
       title: 'E-Motion Rennteam',
       theme: ThemeData(
         primarySwatch: Colors.grey,
-        primaryColor: Colors.grey.shade800,
-        accentColor: Colors.indigoAccent,
+        primaryColor: Color.fromRGBO(26, 26, 26, 1),
+        canvasColor: Color.fromRGBO(19, 19, 19, 1),
+        accentColor: Color.fromRGBO(35, 201, 95, 1),
         visualDensity: VisualDensity.adaptivePlatformDensity,
         textTheme: TextTheme(
           headline1: TextStyle(
             fontWeight: FontWeight.w300,
+            color: Colors.white,
           ),
           headline2: TextStyle(
             fontWeight: FontWeight.w300,
+            color: Colors.white,
           ),
           headline3: TextStyle(
             fontWeight: FontWeight.w300,
+            color: Colors.white,
           ),
           headline4: TextStyle(
             fontWeight: FontWeight.w300,
+            color: Colors.white,
           ),
           headline5: TextStyle(
             fontWeight: FontWeight.w300,
+            color: Colors.white,
+          ),
+          headline6: TextStyle(
+            fontWeight: FontWeight.w300,
+            color: Colors.white,
+          ),
+          button: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+          ),
+          subtitle1: TextStyle(
+            fontWeight: FontWeight.w300,
+            color: Colors.white,
+            fontSize: 15,
           ),
         ),
       ),

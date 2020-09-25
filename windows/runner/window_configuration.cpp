a7e82d74625555301ca8796e8e024d7f871235ce
philenius <philenius@users.noreply.github.com>
2020-09-26 01:13:10 +0200

    prepare release 0.1.0

--- windows/runner/window_configuration.cpp
+++ windows/runner/window_configuration.cpp
@@ -1,6 +1,6 @@
 #include "window_configuration.h"
 
-const wchar_t* kFlutterWindowTitle = L"emotion";
+const wchar_t* kFlutterWindowTitle = L"Log Storage Client";
 const unsigned int kFlutterWindowOriginX = 10;
 const unsigned int kFlutterWindowOriginY = 10;
 const unsigned int kFlutterWindowWidth = 1280;

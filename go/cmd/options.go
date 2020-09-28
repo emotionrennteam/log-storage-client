e9958638e3530c81674e6b546492c0d9e9234174
philenius <philenius@users.noreply.github.com>
2020-09-28 15:39:12 +0200

    register url_launcher as plugin in go-flutter

--- go/cmd/options.go
+++ go/cmd/options.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/go-flutter-desktop/go-flutter"
 	file_picker "github.com/miguelpruivo/flutter_file_picker/go"
+	"github.com/go-flutter-desktop/plugins/url_launcher"
 )
 
 var options = []flutter.Option{
 	flutter.WindowInitialDimensions(1200, 800),
 	flutter.AddPlugin(&file_picker.FilePickerPlugin{}),
+	flutter.AddPlugin(&url_launcher.UrlLauncherPlugin{}),
 	flutter.WindowDimensionLimits(1000, 500, 1920, 1080),
 }
--- go/go.mod
+++ go/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/go-flutter-desktop/go-flutter v0.42.0
+	github.com/go-flutter-desktop/plugins/url_launcher v0.1.2
 	github.com/miguelpruivo/flutter_file_picker/go v0.0.0-20200928124517-0609cae84a4d
 	github.com/pkg/errors v0.9.1
 )

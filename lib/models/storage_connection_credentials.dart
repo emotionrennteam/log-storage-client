9d1d17e0f4611630348172e0d1a8b45d1c2c0468
philenius <philenius@users.noreply.github.com>
2020-10-22 19:56:57 +0200

    add method to valid S3 credentials

--- lib/models/storage_connection_credentials.dart
+++ lib/models/storage_connection_credentials.dart
@@ -9,4 +9,19 @@ class StorageConnectionCredentials {
 
   StorageConnectionCredentials(this.endpoint, this.port, this.region,
       this.bucket, this.accessKey, this.secretKey, this.tlsEnabled);
+
+  bool isValid() {
+    // TODO: extend validation by checking whether the bucket exists
+    return !(this.endpoint == null ||
+        this.endpoint.isEmpty ||
+        this.port == null ||
+        this.region == null ||
+        this.region.isEmpty ||
+        this.bucket == null ||
+        this.bucket.isEmpty ||
+        this.accessKey == null ||
+        this.accessKey.isEmpty ||
+        this.secretKey == null ||
+        this.secretKey.isEmpty);
+  }
 }

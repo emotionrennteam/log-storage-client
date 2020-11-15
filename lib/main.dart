c64b233826eb45812ed10582b8fddd623cad121d
philenius <philenius@users.noreply.github.com>
2020-11-15 18:12:27 +0100

    add lib to dynamically change theme's accent color

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color_theme/dynamic_color_theme.dart';
 import 'package:log_storage_client/utils/locator.dart';
 import 'package:log_storage_client/utils/constants.dart' as constants;
 import 'package:log_storage_client/utils/constants.dart';
@@ -10,71 +11,83 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  ThemeData _buildTheme(Color accentColor, bool isDark) {
+    return ThemeData(
+      primarySwatch: Colors.blueGrey,
+      primaryColor: Color.fromRGBO(32, 33, 37, 1),
+      canvasColor: Color.fromRGBO(22, 23, 27, 1),
+      accentColor: accentColor,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      accentTextTheme: TextTheme(
+        headline5: TextStyle(
+          fontWeight: FontWeight.w300,
+          color: Colors.white,
+        ),
+      ),
+      textTheme: TextTheme(
+        headline1: TextStyle(
+          fontWeight: FontWeight.w300,
+          color: constants.TEXT_COLOR,
+        ),
+        headline2: TextStyle(
+          fontWeight: FontWeight.w300,
+          color: constants.TEXT_COLOR,
+        ),
+        headline3: TextStyle(
+          fontWeight: FontWeight.w300,
+          color: constants.TEXT_COLOR,
+        ),
+        headline4: TextStyle(
+          fontWeight: FontWeight.w300,
+          color: constants.TEXT_COLOR,
+        ),
+        headline5: TextStyle(
+          fontWeight: FontWeight.w300,
+          color: constants.TEXT_COLOR,
+        ),
+        headline6: TextStyle(
+          fontWeight: FontWeight.w400,
+          color: constants.TEXT_COLOR,
+        ),
+        button: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.w400,
+          color: constants.TEXT_COLOR,
+        ),
+        subtitle1: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.w300,
+          color: constants.TEXT_COLOR,
+        ),
+      ),
+      snackBarTheme: SnackBarThemeData(
+        contentTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+      iconTheme: IconThemeData(
+        color: TEXT_COLOR,
+      ),
+      unselectedWidgetColor: Colors.white,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'E-Motion Rennteam',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        primaryColor: Color.fromRGBO(32, 33, 37, 1),
-        canvasColor: Color.fromRGBO(22, 23, 27, 1),
-        accentColor: Color.fromRGBO(1, 176, 117, 1),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        accentTextTheme: TextTheme(
-          headline5: TextStyle(
-            fontWeight: FontWeight.w300,
-            color: Colors.white,
-          ),
-        ),
-        textTheme: TextTheme(
-          headline1: TextStyle(
-            fontWeight: FontWeight.w300,
-            color: constants.TEXT_COLOR,
-          ),
-          headline2: TextStyle(
-            fontWeight: FontWeight.w300,
-            color: constants.TEXT_COLOR,
-          ),
-          headline3: TextStyle(
-            fontWeight: FontWeight.w300,
-            color: constants.TEXT_COLOR,
-          ),
-          headline4: TextStyle(
-            fontWeight: FontWeight.w300,
-            color: constants.TEXT_COLOR,
-          ),
-          headline5: TextStyle(
-            fontWeight: FontWeight.w300,
-            color: constants.TEXT_COLOR,
-          ),
-          headline6: TextStyle(
-            fontWeight: FontWeight.w400,
-            color: constants.TEXT_COLOR,
-          ),
-          button: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.w400,
-            color: constants.TEXT_COLOR,
-          ),
-          subtitle1: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.w300,
-            color: constants.TEXT_COLOR,
-          ),
-        ),
-        snackBarTheme: SnackBarThemeData(
-          contentTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-            fontWeight: FontWeight.w400,
-          ),
-        ),
-        iconTheme: IconThemeData(
-          color: TEXT_COLOR,
-        ),
-        unselectedWidgetColor: Colors.white,
-      ),
-      home: AppLayout(),
+    return DynamicColorTheme(
+      data: this._buildTheme,
+      defaultIsDark: true,
+      // Default AccentColor
+      defaultColor: Color.fromRGBO(1, 176, 117, 1),
+      themedWidgetBuilder: (BuildContext context, ThemeData theme) {
+        return MaterialApp(
+          title: 'E-Motion Rennteam',
+          theme: theme,
+          home: AppLayout(),
+        );
+      },
     );
   }
 }

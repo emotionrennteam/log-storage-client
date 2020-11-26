8517dedfdd63d24df66e62164ccf4e0458c582b0
philenius <philenius@users.noreply.github.com>
2020-11-26 21:39:10 +0100

    add additional unit tests f. utils.getParentPath()

--- test/utils_test.dart
+++ test/utils_test.dart
@@ -4,7 +4,8 @@ import 'package:path/path.dart' as path;
 
 void main() {
   group('getParentPath()', () {
-    test('returns the correct path for a path that ends w/ a slash on Linux', () {
+    test('returns the correct path for a path that ends w/ a slash on Linux',
+        () {
       final currentPath = '/home/phil/Downloads/';
 
       final result = utils.getParentPath(currentPath, path.separator);
@@ -36,6 +37,48 @@ void main() {
       final result = utils.getParentPath(currentPath, path.separator);
 
       expect(result, '/');
+    }, testOn: 'linux');
+
+    test('returns the correct parent path when log root dir is set',
+        () {
+      final logRootDir = '/etc/logs/telemetry-logs';
+      final currentPath = '/etc/logs/telemetry-logs/2020-11-26-workshop-test/part-a';
+
+      final result = utils.getParentPath(
+        currentPath,
+        path.separator,
+        artificialRootDirectory: logRootDir,
+      );
+
+      expect(result, '/etc/logs/telemetry-logs/2020-11-26-workshop-test');
+    }, testOn: 'linux');
+
+    test('prevents requesting the parent dir when parent dir would be "above" log root log',
+        () {
+      final logRootDir = '/etc/logs/telemetry-logs';
+      final currentPath = '/etc/logs/telemetry-logs';
+
+      final result = utils.getParentPath(
+        currentPath,
+        path.separator,
+        artificialRootDirectory: logRootDir,
+      );
+
+      expect(result, logRootDir);
+    }, testOn: 'linux');
+
+    test('prevents requesting the parent dir when parent dir would be "above" log root log',
+        () {
+      final logRootDir = '/etc/logs/telemetry-logs';
+      final currentPath = '/etc/logs/';
+
+      final result = utils.getParentPath(
+        currentPath,
+        path.separator,
+        artificialRootDirectory: logRootDir,
+      );
+
+      expect(result, logRootDir);
     }, testOn: 'linux');
 
     test(

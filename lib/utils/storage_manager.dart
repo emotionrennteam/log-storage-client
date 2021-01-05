4ea72885c49cc054c53dfdc2010ed9a8223c56d5
philenius <philenius@users.noreply.github.com>
2021-01-05 15:14:30 +0100

    fix function to sort files alphabeticallly
    Previously, the alphabetical sorting was case-sensitive which made all
    uppercase file names appear before all lowercase file names. Now, all
    file names are turned into lowercase to prevent this separation. Also,
    the special file '_metadata.json'  is sorted so that it always appear as
    the very first file.

--- lib/utils/storage_manager.dart
+++ lib/utils/storage_manager.dart
@@ -366,16 +366,33 @@ Future<void> uploadObjectsToRemoteStorage(
 
 /// Custom sorting which sorts a [List] of [StorageObject]s by directories first
 /// followed by all files. Within the set of directories and files, all elements
-/// are sorted in alphabetically descending order.
+/// are sorted in alphabetically descending order with the exception of the special
+/// file `_metadata.json`. This file is sorted so that it is always shown as the
+/// very first file.
 void sortByDirectoriesFirstThenFiles(List<StorageObject> listToSort) {
   listToSort.sort((StorageObject o1, StorageObject o2) {
-    int compare = (o1.isDirectory == o2.isDirectory)
-        ? 0
-        : o1.isDirectory
-            ? -1
-            : 1;
-    if (compare != 0) return compare;
-    return o1.getBasename().compareTo(o2.getBasename());
+    if (o1.isDirectory && o2.isDirectory) {
+      return o1
+          .getBasename()
+          .toLowerCase()
+          .compareTo(o2.getBasename().toLowerCase());
+    }
+    if (o1.isDirectory) {
+      return -1;
+    }
+    if (o2.isDirectory) {
+      return 1;
+    }
+    if (o1.getBasename() == '_metadata.json') {
+      return -1;
+    }
+    if (o2.getBasename() == '_metadata.json') {
+      return 1;
+    }
+    return o1
+        .getBasename()
+        .toLowerCase()
+        .compareTo(o2.getBasename().toLowerCase());
   });
 }
 
